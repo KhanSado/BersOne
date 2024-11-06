26e14022b915cdf0ea81311dd1320097df17042a
bersons <bersonintech@gmail.com>
2024-11-06 15:30:30 -0300

    ajust env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,5 +17,5 @@ export const environment = {
     messagingSenderId: "740394568542",
     appId: "1:740394568542:web:759223c9edf3c452a05dcf",
     measurementId: "G-BXH1GCP2VN"
-  };
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -22,7 +22,7 @@ export const environment = {
     messagingSenderId: "740394568542",
     appId: "1:740394568542:web:759223c9edf3c452a05dcf",
     measurementId: "G-BXH1GCP2VN"
-  };
+  }
 };
 
 /*
