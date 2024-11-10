271fe6ddb2959a1c345f0411f3389b1521f05136
bersons <bersonintech@gmail.com>
2024-11-10 17:00:14 -0300

    ajuste de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: '/auth/signin',
+        redirectTo: '/blog/home',
         pathMatch: 'full'
       },
       {
